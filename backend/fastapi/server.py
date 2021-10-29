5eaa9a831fdfb9dc44b4ba50a056da9c18aff122
Shun Takeda <shun9646@gmail.com>
2021-10-29 15:05:41 +0000

    WIP:web server endpoint

--- backend/fastapi/server.py
+++ backend/fastapi/server.py
@@ -1,20 +1,31 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request, File, UploadFile
+import cv2
 
 
-def get_application():
-    app = FastAPI(title="Hedgehog Reservation", version="1.0.0")
 
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+app = FastAPI()
 
-    return app
+@app.post("/files/")
+async def create_file(file: bytes = File(...)):
+    return {"file_size": len(file)}
 
-app = get_application()
+@app.post("/api/objectdetection")
+async def object_detection_file(file: UploadFile = File(...)):
+    return {"filename": len(file)}
+
+@app.post("/api/searchlabel")
+async def serach_file_labels(file: UploadFile = File(...)):
+    return {"api": len(file)}
+
+
+#データを画像に変換する
+def read_image(bin_data, size=(256,256)):
+    file_bytes = np.asarray(bytearray(bin_data.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, size)
+    return img    
 
 
