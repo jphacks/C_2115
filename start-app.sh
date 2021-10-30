4a1fd0fef45ed3d1dcd67e727a884efe7f7bdc05
Shun Takeda <shun9646@gmail.com>
2021-10-30 02:49:07 +0000

    fixed doceker-compose.yml and requirements.txt

--- start-app.sh
+++ start-app.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker-compose build
-docker-compose run --rm node sh -c "npm install -g create-react-app && create-react-app react-front"
+docker-compose run --rm node sh -c "npm install -g create-react-app && create-react-app react-front && npm install @material-ui/core && npm install @mui/material@emotion/react@emotion/styled"
 docker-compose up -d
